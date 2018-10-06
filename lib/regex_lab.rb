30c750991d4642ca55f8bff4f42fb6abb70310d6
David masterson <david73180@gmail.com>
2018-10-06 22:09:00 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,26 @@
 def starts_with_a_vowel?(word)
+if word.match(/^[aeiou]/)
+  return true
+end
 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+   text.match(/\wun+ing/)
 
 end
 
 def words_five_letters_long(text)
+  grep(/\b\w{5}\b/)
 
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
+  return text.match()
 
 end
 
 def valid_phone_number?(phone)
+  return phone.scan()
 
 end
